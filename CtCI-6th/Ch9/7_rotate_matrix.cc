1067a760c759618f6185f07ea470a5b89f885782
lys861205 <lys86_1205@126.com>
2020-08-06 11:18:59 +0800

    commit CtCI

--- CtCI-6th/Ch9/7_rotate_matrix.cc
+++ CtCI-6th/Ch9/7_rotate_matrix.cc
@@ -12,9 +12,10 @@ void RotateMatrix(vector<vector<int> >& matrix, int M, int N)
   for (int i=0; i < M/2; ++i) {
     for (int j=i; j < N-i-1; ++j) {
       int temp =  matrix[i][j];
-      matrix[i][j] = matrix[N-i-1][N-j-1];
-      matrix[N-i-1][N-j-1] = matrix[N-j-1][i];
-      matrix[N-j-1][i] = temp;
+      matrix[i][j] = matrix[N-j-1][i];
+      matrix[N-j-1][i] = matrix[N-1-i][N-1-j];
+      matrix[N-i-1][N-j-1] = matrix[j][N-1-i];
+      matrix[j][N-1-i] = temp;
     }
   }
 }
@@ -26,6 +27,14 @@ int main()
     {3, 4, 5},
     {6, 7, 8}
   };
+  printf("旋转前\n");
+  for (int i=0; i < matrix.size(); ++i) {
+    for (int j=0; j < matrix[i].size(); ++j) {
+      printf("%d ", matrix[i][j]);
+    }
+    printf("\n");
+  }
+  printf("旋转后\n");
   RotateMatrix(matrix, 3, 3);
   for (int i=0; i < matrix.size(); ++i) {
     for (int j=0; j < matrix[i].size(); ++j) {
