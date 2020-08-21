92f31d18fb5c23a6d0f04c8ecac0b50fc5a6134c
lys861205 <lys86_1205@126.com>
2020-08-21 18:03:11 +0800

    redis cluster client

---
+++ redis_cluster.py
@@ -0,0 +1,190 @@
+#!/bin/python
+
+from redis import *
+import time
+
+crc16tab = [
+  0x0000,0x1021,0x2042,0x3063,0x4084,0x50a5,0x60c6,0x70e7,
+  0x8108,0x9129,0xa14a,0xb16b,0xc18c,0xd1ad,0xe1ce,0xf1ef,
+  0x1231,0x0210,0x3273,0x2252,0x52b5,0x4294,0x72f7,0x62d6,
+  0x9339,0x8318,0xb37b,0xa35a,0xd3bd,0xc39c,0xf3ff,0xe3de,
+  0x2462,0x3443,0x0420,0x1401,0x64e6,0x74c7,0x44a4,0x5485,
+  0xa56a,0xb54b,0x8528,0x9509,0xe5ee,0xf5cf,0xc5ac,0xd58d,
+  0x3653,0x2672,0x1611,0x0630,0x76d7,0x66f6,0x5695,0x46b4,
+  0xb75b,0xa77a,0x9719,0x8738,0xf7df,0xe7fe,0xd79d,0xc7bc,
+  0x48c4,0x58e5,0x6886,0x78a7,0x0840,0x1861,0x2802,0x3823,                                                                                                             
+  0xc9cc,0xd9ed,0xe98e,0xf9af,0x8948,0x9969,0xa90a,0xb92b,
+  0x5af5,0x4ad4,0x7ab7,0x6a96,0x1a71,0x0a50,0x3a33,0x2a12,
+  0xdbfd,0xcbdc,0xfbbf,0xeb9e,0x9b79,0x8b58,0xbb3b,0xab1a,
+  0x6ca6,0x7c87,0x4ce4,0x5cc5,0x2c22,0x3c03,0x0c60,0x1c41,
+  0xedae,0xfd8f,0xcdec,0xddcd,0xad2a,0xbd0b,0x8d68,0x9d49,
+  0x7e97,0x6eb6,0x5ed5,0x4ef4,0x3e13,0x2e32,0x1e51,0x0e70,
+  0xff9f,0xefbe,0xdfdd,0xcffc,0xbf1b,0xaf3a,0x9f59,0x8f78,
+  0x9188,0x81a9,0xb1ca,0xa1eb,0xd10c,0xc12d,0xf14e,0xe16f,
+  0x1080,0x00a1,0x30c2,0x20e3,0x5004,0x4025,0x7046,0x6067,
+  0x83b9,0x9398,0xa3fb,0xb3da,0xc33d,0xd31c,0xe37f,0xf35e,
+  0x02b1,0x1290,0x22f3,0x32d2,0x4235,0x5214,0x6277,0x7256,
+  0xb5ea,0xa5cb,0x95a8,0x8589,0xf56e,0xe54f,0xd52c,0xc50d,
+  0x34e2,0x24c3,0x14a0,0x0481,0x7466,0x6447,0x5424,0x4405,
+  0xa7db,0xb7fa,0x8799,0x97b8,0xe75f,0xf77e,0xc71d,0xd73c,
+  0x26d3,0x36f2,0x0691,0x16b0,0x6657,0x7676,0x4615,0x5634,
+  0xd94c,0xc96d,0xf90e,0xe92f,0x99c8,0x89e9,0xb98a,0xa9ab,
+  0x5844,0x4865,0x7806,0x6827,0x18c0,0x08e1,0x3882,0x28a3,
+  0xcb7d,0xdb5c,0xeb3f,0xfb1e,0x8bf9,0x9bd8,0xabbb,0xbb9a,
+  0x4a75,0x5a54,0x6a37,0x7a16,0x0af1,0x1ad0,0x2ab3,0x3a92,
+  0xfd2e,0xed0f,0xdd6c,0xcd4d,0xbdaa,0xad8b,0x9de8,0x8dc9,
+  0x7c26,0x6c07,0x5c64,0x4c45,0x3ca2,0x2c83,0x1ce0,0x0cc1,
+  0xef1f,0xff3e,0xcf5d,0xdf7c,0xaf9b,0xbfba,0x8fd9,0x9ff8,
+  0x6e17,0x7e36,0x4e55,0x5e74,0x2e93,0x3eb2,0x0ed1,0x1ef0
+]
+
+def crc16(buf, l):
+  crc = 0
+  for i in range(l):
+    crc = (crc<<8) ^ crc16tab[((crc>>8) ^ ord(buf[i]))&0x00FF]
+    crc &= 0xFFFF
+  return crc
+
+class BitSet(object):
+  slots = 16384
+  def __init__(self):
+    self.bitmap = []
+    for i in range(BitSet.slots//8):
+      self.bitmap.append(0)
+  
+  def set(self, number):
+    self.bitmap[number >> 3] |= (1 << (number & 7))
+  
+  def clear(self, number):
+    self.bitmap[number >> 3] &= ~(1 << (number & 7))
+
+  def exist(self, number):
+    return (self.bitmap[number >> 3] & (1 << (number & 7)) != 0)
+
+  def debug(self):
+    n = 0
+    for slot in self.bitmap:
+      if slot & 0x40:
+        print (n*8 + 7),
+      if slot & 0x20:
+        print (n*8 + 6),
+      if slot & 0x10:
+        print (n*8 + 5),
+      if slot & 0x08:
+        print (n*8 + 4),
+      if slot & 0x04:
+        print (n*8 + 3),
+      if slot & 0x02:
+        print (n*8 + 2),
+      if slot & 0x01:
+        print (n*8 + 1),
+      n = n+1
+
+class ClusterData(object):
+  def __init__(self, data):
+    info = data.split(" ")
+    self.node_id = info[0]
+    self.__parse_host(info[1])
+    self.__parse_type(info[2])
+    self.__parse_slot(info)
+
+  def __parse_slot(self, slots):
+    if not self.master:
+      return
+    self.bitset = BitSet()
+    for i in range(8, len(slots)):
+      slot_list = slots[i].split('-')
+      lo = int(slot_list[0])
+      hi = int(slot_list[1])+1
+      for slot in range(lo, hi):
+        self.bitset.set(slot)
+     
+  def __parse_host(self, hoststr):
+    hoststr = hoststr.split("@")[0]
+    ip_port = hoststr.split(":")
+    self.host = ip_port[0]
+    self.port = int(ip_port[1])
+
+  def __parse_type(self, typestr):
+    if -1 == typestr.find("master"):
+      self.master = False
+    else:
+      self.master = True
+
+  def Host(self):
+    return self.host
+
+  def Port(self):
+    return self.port
+
+  def set_context(self, ctx):
+    self.context = ctx
+
+  def Context(self):
+    return self.context
+
+  def is_master(self):
+    return self.master
+
+  def has_slot(self, slot):
+    return self.bitset.exist(slot)
+
+  def debug(self):
+    return self.bitset.debug()
+
+  def __str__(self):
+    return "[%s:%d]" % (self.host, self.port)
+
+
+class RedisCluster(Redis):
+  """
+  Implementation of the Redis cluster.
+  """
+  def __init__(self, host="localhost", port=6379,
+               db=0, max_connections=None):
+     c = StrictRedis(host, port, max_connections=5)
+     cluster_nodes = c.execute_command("cluster nodes")
+     self.nodes = []
+
+     for line in cluster_nodes.splitlines():
+        node = ClusterData(line) 
+        if node.is_master():
+          self.nodes.append(node)
+
+     self.__connect_all_master()
+
+  def __connect_all_master(self):
+    self.clusters = {}
+    for i in range(len(self.nodes)):
+      node = self.nodes[i]
+      ctx = StrictRedis(node.Host(), node.Port())
+      node.set_context(ctx)
+      self.clusters[str(node)] = node
+    
+  def __pick_redis(self, key):
+    crc = crc16(key, len(key)) 
+    slot = crc & (16384-1)
+    for cluster in self.clusters.values():
+      if cluster.has_slot(slot):
+        return cluster.Context()
+
+    return None
+    
+  def set(self, name, value):
+    redis_cli = self.__pick_redis(name)
+    return redis_cli.set(name, value)
+
+  def get(self, name):
+    redis_cli = self.__pick_redis(name)
+    return redis_cli.get(name)
+
+rc = RedisCluster(host="10.139.48.96", port=7000)
+
+d = {'1':'java', '2': 'C++', '3': 'lua', '10':'Go', '11':'python'}
+
+for k in d:
+  rc.set(k, d[k])
+
+for k in d:
+  print rc.get(k)
+
